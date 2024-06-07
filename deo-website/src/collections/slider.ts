650322a95a962dde1ef5f6f3fb83dc1b6dd7c9fd
cmsusergit <cmsusergit@gmail.com>
2024-06-07 14:59:11 +0530

    test commit

---
+++ deo-website/src/collections/slider.ts
@@ -0,0 +1,44 @@
+import { CollectionConfig } from 'payload/types'
+import { RowLabelArgs } from 'payload/dist/admin/components/forms/RowLabel/types'
+
+export const ExampleCollection: CollectionConfig = {
+  slug: 'example-collection',
+  fields: [
+    {
+      name: 'slider', // required
+      type: 'array', // required
+      label: 'Image Slider',
+      minRows: 2,
+      maxRows: 10,
+      interfaceName: 'CardSlider', // optional
+      labels: {
+        singular: 'Slide',
+        plural: 'Slides',
+      },
+      fields: [
+        // required
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+          required: true,
+        },
+        {
+          name: 'caption',
+          type: 'text',
+        },
+      ],
+      admin: {
+        components: {
+          RowLabel: ({ data, index }: RowLabelArgs) => {
+            return data?.title || `Slide ${String(index).padStart(2, '0')}`
+          },
+        },
+      },
+    },
+  ],
+}
---
+++ test1/adorable-atmosphere/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 4040,
+    host: true
+  },
+  integrations: [sanity({
+    projectId: "9kplpxfq",
+    dataset: "testproject",
+    useCdn: false
+  }), react(), tailwind()]
+});
