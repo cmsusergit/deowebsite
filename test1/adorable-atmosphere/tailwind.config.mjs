88de715068600633e0b3c74ac35c0f8507cb06a3
cmsusergit <cmsusergit@gmail.com>
2024-06-08 10:04:10 +0530

    test done

--- test1/adorable-atmosphere/tailwind.config.mjs
+++ test1/adorable-atmosphere/tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'node_modules/preline/dist/*.js'
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+
+	],
 }
+
+
