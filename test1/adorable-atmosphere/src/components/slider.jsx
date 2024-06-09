58d8b8c436ab7e1bf9a1f36e4b87f2aaad0238aa
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2024-06-09 12:02:27 +0000

    test done

--- test1/adorable-atmosphere/src/components/slider.jsx
+++ test1/adorable-atmosphere/src/components/slider.jsx
@@ -1,48 +1,75 @@
-import { useState,useEffect } from 'react';
-export default function Slider({sliderList}){
-  
-  const [currindx,setCurrIndx]=useState(0)
-  return (  
-  <>
-    <div className="relative w-full bg-gray-100 min-h-72 border">
-      <p>----{JSON.stringify(sliderList)}----</p>
-      {
+import { useState, useEffect } from "react";
+export default function Slider({ sliderList }) {
+  const [currindx, setCurrIndx] = useState(0);
+  return (
+    <>
+      <div className="min-h-72 bg-[#87D2D0] w-full shadow-lg shadow-slate-800 relative">
+        <p>----{JSON.stringify(sliderList)}----</p>
+        {
           // sliderList.map((slider,indx)=>{
           //     return indx==currindx && (
-          //     <div key={indx} className="absolute left-0 top-0 bg-blue-400 min-h-72 w-full text-white text-center">                    
+          //     <div key={indx} className="absolute left-0 top-0 bg-blue-400 min-h-72 w-full text-white text-center">
           //           {JSON.stringify(slider)}
-
-          //           {/* <img src={slider.image} alt={slider.alt}/> 
+          //           {/* <img src={slider.image} alt={slider.alt}/>
           //           */}
-
           //           <p>----{JSON.stringify(slider)}----</p>
           //     </div>
           //     )
           // })
-      }      
-      <button onClick={()=>{setCurrIndx(currindx>0?(currindx-1):(sliderList.length-1))}} id="prevBtn" type="button" className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white">
+        }
+        <button
+          onClick={() => {
+            setCurrIndx(currindx > 0 ? currindx - 1 : sliderList.length - 1);
+          }}
+          id="prevBtn"
+          type="button"
+          className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white"
+        >
           <span className="text-2xl" aria-hidden="true">
-            <svg className="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              className="flex-shrink-0 size-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <path d="m15 18-6-6 6-6"></path>
             </svg>
           </span>
           <span className="sr-only">Previous</span>
         </button>
-        <button onClick={()=>{setCurrIndx((currindx+1)%sliderList.length)}} id="nextBtn" type="button" className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white">
+        <button
+          onClick={() => {
+            setCurrIndx((currindx + 1) % sliderList.length);
+          }}
+          id="nextBtn"
+          type="button"
+          className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white"
+        >
           <span className="sr-only">Next</span>
           <span className="text-2xl" aria-hidden="true">
-            <svg className="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              className="flex-shrink-0 size-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <path d="m9 18 6-6-6-6"></path>
             </svg>
           </span>
         </button>
-    </div></>
-)}  
-
-
-
-
-
-
-
-
+      </div>
+    </>
+  );
+}
--- test1/adorable-atmosphere/tailwind.config.mjs
+++ test1/adorable-atmosphere/tailwind.config.mjs
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      poppin: ["Poppins", "sans-serif"],
+      body: ["poppin"],
+      display: ["poppin"],
+    },
+    extend: {
+      keyframes: {
+        tilt: {
+          "0%,100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(5deg)" },
 
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'node_modules/preline/dist/*.js'
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('preline/plugin'),
-
-	],
-}
-
-
+          "75%": { transform: "rotate(-5deg)" },
+        },
+      },
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
